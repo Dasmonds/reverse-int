dbf1dcd0fddfd032c09520b8c9610b92e69c07c0
Yokeer <Alexanderdlack34@gmail.com>
2022-06-26 12:23:23 +0300

    feat: task comlete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (number) {
+
+    let reverseNumber = number.toString().split('')
+    if (reverseNumber[0] !== '-') {
+        return reverseNumber.reverse().join('')
+    } else {
+        return reverseNumber.reverse().splice(0, 3).join('')
+    }
 }
